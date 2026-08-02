7ffa68d2e80cf0b328cbbe8f87d19edf80712157
Danny <dainal.sabouri@gmail.com>
2026-08-02 18:22:41 +1200

    add a real query

--- app/main.py
+++ app/main.py
@@ -5,6 +5,6 @@ app = FastAPI()
 
 @app.get("/sampleData")
 async def hello():
-    data = executeQuery("SELECT * FROM cigarette_smoking limit 5")
+    data = executeQuery("select * from cigarette_smoking where cen23_geo_008 = '9999' and cen23_cig_002 = '01' and cen23_eth_004 = '12913' and cen23_age_008 = '2'")
 
     return data
